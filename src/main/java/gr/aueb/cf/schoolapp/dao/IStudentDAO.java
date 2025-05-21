5f710af9e6bb9397237556bb161f8717808eb8e4
Nikolaos Theofanis <156532831+nickTheof@users.noreply.github.com>
2025-05-21 13:19:26 +0300

    chore: implement deleteByUuid in student dao

--- src/main/java/gr/aueb/cf/schoolapp/dao/IStudentDAO.java
+++ src/main/java/gr/aueb/cf/schoolapp/dao/IStudentDAO.java
@@ -3,4 +3,5 @@ package gr.aueb.cf.schoolapp.dao;
 import gr.aueb.cf.schoolapp.model.Student;
 
 public interface IStudentDAO extends IGenericDAO<Student> {
+    void deleteByUuid(String uuid);
 }
--- src/main/java/gr/aueb/cf/schoolapp/dao/StudentDAOImpl.java
+++ src/main/java/gr/aueb/cf/schoolapp/dao/StudentDAOImpl.java
@@ -2,10 +2,18 @@ package gr.aueb.cf.schoolapp.dao;
 
 import gr.aueb.cf.schoolapp.model.Student;
 import jakarta.enterprise.context.ApplicationScoped;
+import jakarta.persistence.EntityManager;
 
 @ApplicationScoped
 public class StudentDAOImpl extends AbstractDAO<Student> implements IStudentDAO {
     public StudentDAOImpl() {
         this.setPersistenceClass(Student.class);
     }
+
+    @Override
+    public void deleteByUuid(String uuid) {
+        EntityManager em = getEntityManager();
+        getByField("uuid", uuid).ifPresent(em::remove);
+    }
+
 }
