a24584d818213f7faeaa93c4131093bc741509b2
Nikolaos Theofanis <156532831+nickTheof@users.noreply.github.com>
2025-05-22 14:03:42 +0300

    feat: implement deleteByUuid method in CityDAO

--- src/main/java/gr/aueb/cf/schoolapp/dao/CityDAOImpl.java
+++ src/main/java/gr/aueb/cf/schoolapp/dao/CityDAOImpl.java
@@ -2,10 +2,17 @@ package gr.aueb.cf.schoolapp.dao;
 
 import gr.aueb.cf.schoolapp.model.City;
 import jakarta.enterprise.context.ApplicationScoped;
+import jakarta.persistence.EntityManager;
 
 @ApplicationScoped
 public class CityDAOImpl extends AbstractDAO<City> implements ICityDAO{
     public CityDAOImpl() {
         this.setPersistenceClass(City.class);
+    }
+
+    @Override
+    public void deleteByUuid(String uuid) {
+        EntityManager em = getEntityManager();
+        getByField("uuid", uuid).ifPresent(em::remove);
     }
 }
--- src/main/java/gr/aueb/cf/schoolapp/dao/ICityDAO.java
+++ src/main/java/gr/aueb/cf/schoolapp/dao/ICityDAO.java
@@ -3,4 +3,5 @@ package gr.aueb.cf.schoolapp.dao;
 import gr.aueb.cf.schoolapp.model.City;
 
 public interface ICityDAO extends IGenericDAO<City> {
+    void deleteByUuid(String uuid);
 }
