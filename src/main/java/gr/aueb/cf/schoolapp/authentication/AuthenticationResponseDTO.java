0e94921cf492e0720893d722be4af7bdfde6dcec
Nikolaos Theofanis <156532831+nickTheof@users.noreply.github.com>
2025-05-22 16:41:14 +0300

    feat: add Authentication Rest Controller and implement authentication jwt service, securityContext and filter. Add auth response DTO.

---
+++ src/main/java/gr/aueb/cf/schoolapp/authentication/AuthenticationResponseDTO.java
@@ -0,0 +1,14 @@
+package gr.aueb.cf.schoolapp.authentication;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class AuthenticationResponseDTO {
+    private String token;
+}
