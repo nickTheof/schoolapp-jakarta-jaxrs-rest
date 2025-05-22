3c2940729aed51789d440cbe6eb8f4d4a26e89d6
Nikolaos Theofanis <156532831+nickTheof@users.noreply.github.com>
2025-05-22 15:39:49 +0300

    fix: add no-args constructors (needed by jackson)

--- src/main/java/gr/aueb/cf/schoolapp/dto/StudentInsertDTO.java
+++ src/main/java/gr/aueb/cf/schoolapp/dto/StudentInsertDTO.java
@@ -1,5 +1,8 @@
 package gr.aueb.cf.schoolapp.dto;
 
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor
 public class StudentInsertDTO extends PersonInsertDTO{
     public StudentInsertDTO(String firstname, String lastname, String vat, String email, String cityUuid) {
         super(firstname, lastname, vat, email, cityUuid);
--- src/main/java/gr/aueb/cf/schoolapp/dto/StudentUpdateDTO.java
+++ src/main/java/gr/aueb/cf/schoolapp/dto/StudentUpdateDTO.java
@@ -1,5 +1,8 @@
 package gr.aueb.cf.schoolapp.dto;
 
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor
 public class StudentUpdateDTO extends PersonUpdateDTO{
     public StudentUpdateDTO(String uuid, String firstname, String lastname, String email, String cityUuid) {
         super(uuid, firstname, lastname, email, cityUuid);
